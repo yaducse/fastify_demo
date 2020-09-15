8d58122253ac337a8642b0224cbedec823379a47
Yadu B <yadu.b@ad.infosys.com>
2020-09-15 16:56:37 +0530

    initial-commit

---
+++ our-first-route.js
@@ -0,0 +1,66 @@
+const Ajv = require('ajv');
+const ajv = new Ajv({
+  coerceTypes: false
+})
+async function routes(fastify,options) {
+
+    const opts1 = {
+        schema: {
+          response: {
+            200: {
+              type: 'object',
+              properties: {
+                GET: { type: 'string' }
+              }
+            }
+          }
+        }
+      }
+
+    fastify.get('/', async (request,reply) => {
+       
+        return { GET: "works"}
+    })
+
+  const opts2 = {
+    schema : {
+      body : {
+        type: 'object',
+        additionalProperties: false,
+        maxProperties: 2,
+        required: [
+          'someKey',
+          'someOtherKey'
+        ],
+        properties: {
+          someKey: { type: 'number' },
+          someOtherKey: { type: 'string' }
+        }
+      },
+      validatorCompiler: ({ schema, method, url, httpPart }) => {
+        return data => ajv.validate(opts2)
+      }
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: {
+          someKey: { type: 'string'},
+          someOtherKey: { type: 'number'}
+        }
+      }
+    }
+
+  }
+    
+    fastify.post('/', opts2, async (request, reply) => {
+      
+        reply.code(201);
+        reply.send( { POST: 'works' });
+      
+      
+    })
+ 
+}
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,30 @@
+
+const fastify = require('fastify')({ 
+    logger: true
+});
+
+// fastify.get('/', async (request, reply) => { 
+//     reply.send({Yadu: 'B'})
+// });
+
+fastify.register(require('./our-first-route'));
+
+fastify.listen(3000, (err,address) => {
+    if(err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(
+        'Server listening on '+address
+    )
+});
+
+/* const start = async () => {
+    try{
+        await fastify.listen(3000);
+    }   catch(err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start(); */
